1604592ff5f51e14fd2bdd6dc3bf15da9fe67f94
Ivan Jenkac <ivanjenkac@gwsteam.com>
2021-06-30 11:16:17 +0100

    Added healthcheck to router service in compose and updated rebuild script for quick testing of builds

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash 
-docker stop $(docker ps | grep "sprint-name-generator-web:latest" | cut -d" " -f1)
+container_id=$(docker ps | grep "sprint-name-generator-web:latest" | cut -d" " -f1)
+[ ! -z "$container_id" ] && docker stop $container_id
 docker build -t sprint-name-generator-web:latest .
 docker run -d -p 80:80 sprint-name-generator-web:latest
